38cd8f03916dc0a2c98797a850d3e02abc9f8728
sayymeer <sameer.yadv8@gmail.com>
2023-12-07 07:34:20 +0530

    updated apis

--- aptos.py
+++ aptos.py
@@ -11,6 +11,12 @@ load_dotenv()
 
 NODE_URL = os.getenv("APTOS_NODE_URL")
 PRIV_KEY = os.getenv("ACC_PRIV_KEY")
+MOD_NAME = os.getenv("MOD_NAME")
+EXP_ORD_FUNC = os.getenv("EXP_ORD_FUNC")
+CHANGE_J_FUNC = os.getenv("CHANGE_J_FUNC")
+MARKET_PRICE_FUNC = os.getenv("MARKET_PRICE_FUNC")
+PLACE_ORDER_FUNC = os.getenv("PLACE_ORDER_FUNC")
+
 
 rest_client = RestClient(NODE_URL)
 account = Account.load_key(PRIV_KEY)
@@ -20,14 +26,14 @@ async def autoCall():
     '''this function run 2 bcs transactions'''
     trans_arg_1 = [TransactionArgument(date.today().day,Serializer.u64)]
     payload1 = TransactionPayload(EntryFunction.natural(
-        f'{account.account_address}::SUCC',
-        "getbuylist",
+        f'{str(account.account_address)}::{MOD_NAME}',
+        f"{EXP_ORD_FUNC}",
         [],
         trans_arg_1
     ))
     payload2 = TransactionPayload(EntryFunction.natural(
-        f'{account.account_address}::SUCC',
-        'changeJ',
+        f'{str(account.account_address)}::{MOD_NAME}',
+        f'{CHANGE_J_FUNC}',
         [],
         []
     ))
@@ -37,7 +43,23 @@ async def autoCall():
     await rest_client.submit_bcs_transaction(sign_trans_2)
     return
 
-async def RiskData(module:str,func:str):
+async def RiskData():
     '''this function returns data for risk calculation'''
-    res = await rest_client.account_resource(account.account_address,f"{account.account_address}::{module}::{func}")
-    return res.data
+    res = await rest_client.account_resource(account.account_address,f"{account.account_address}::{MOD_NAME}::{MARKET_PRICE_FUNC}")
+    return res['data']['data']
+
+
+async def placeOrder(privKey:str,amount:int,price:int,timestamp:int,date:int,side:bool,leverage:int):
+    account = Account.load_key(privKey)
+    trans_arg = [TransactionArgument(amount,Serializer.u64),TransactionArgument(price,Serializer.u64),TransactionArgument(timestamp,Serializer.u64),TransactionArgument(date,Serializer.u64),TransactionArgument(side,Serializer.bool),TransactionArgument(leverage,Serializer.u64)]
+    payload = TransactionPayload(
+        EntryFunction.natural(
+            f'{str(account.account_address)}::{MOD_NAME}',
+            f"{PLACE_ORDER_FUNC}",
+            [],
+            trans_arg
+        )
+    )
+    sign_trans = await rest_client.create_bcs_signed_transaction(account,payload)
+    await rest_client.submit_bcs_transaction(sign_trans)
+    return
--- main.py
+++ main.py
@@ -1,14 +1,32 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from aptos import autoCall,RiskData
+from aptos import autoCall,RiskData,placeOrder
 from market import GetRiskStats
+from pydantic import BaseModel
 
 scheduler = AsyncIOScheduler()
 scheduler.add_job(autoCall,'cron',hour=23,minute=59)
 scheduler.start()
 
 app = FastAPI()
+app.add_middleware(CORSMiddleware,allow_origins='*')
+
+data = []
+
+class PlaceOrderBody(BaseModel):
+    privKey:str
+    amount:int
+    price:int
+    timestamp:int
+    date:int
+    side:bool
+    leverage:int
+
+async def UpdateData():
+    global data
+    data = await RiskData()
 
 @app.get('/')
 async def root():
@@ -18,9 +36,22 @@ async def root():
 
 @app.get('/riskStat')
 async def riskStat():
-    data = await RiskData("Coin1","MarketPrice")
-    return GetRiskStats(data)
+    return GetRiskStats(list(map(int,data)))
 
+
+@app.get('/update')
+async def marketprice():
+    await UpdateData()
+    return {'data':data}
+
+@app.get('/view')
+async def view():
+    return {'data':data}
+
+@app.post('/placeOrder')
+async def placeOrderHandler(plc:PlaceOrderBody):
+    await placeOrder(plc.privKey,plc.amount,plc.price,plc.timestamp,plc.date,plc.side,plc.leverage)
+    return "success"
 
 
 if __name__ == '__main__':
